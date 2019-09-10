2acf36e19c61db2b690972bd879f3fc4ab2a337d
Isaiah Boren <MistaZayuh@gmail.com>
2019-09-10 13:50:01 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Semantic from "semantic-ui-react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class FlashCards extends React.Component {
+  state = { Flashcards: [
+    { id: 1, front: "", back: "",}
+    { id: 2, front: "", back: "",}
+    { id: 3, front: "", back: "",}
+  ]};
 
-export default App;
+
+
+
+
+
+  render () {
+    return (
+      <div>
+        
+      </div>
+    );
+  };
+};
+
+export default FlashCards;
